a26ad3c77fa1545f434f69c29bf634438864f7b8
JOSELUISLOPEZDIAZ <joseluislopezdiaz78@gmail.com>
2020-10-14 22:57:21 -0400

    cambio 5

--- pruebaGit/pruebaGit/Form3.Designer.vb
+++ pruebaGit/pruebaGit/Form3.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Form3
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.MonthCalendar1 = New System.Windows.Forms.MonthCalendar()
         Me.SuspendLayout()
         '
         'Button1
@@ -42,11 +43,18 @@ Partial Class Form3
         Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
         Me.DateTimePicker1.TabIndex = 1
         '
+        'MonthCalendar1
+        '
+        Me.MonthCalendar1.Location = New System.Drawing.Point(39, 139)
+        Me.MonthCalendar1.Name = "MonthCalendar1"
+        Me.MonthCalendar1.TabIndex = 2
+        '
         'Form3
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ClientSize = New System.Drawing.Size(320, 330)
+        Me.Controls.Add(Me.MonthCalendar1)
         Me.Controls.Add(Me.DateTimePicker1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form3"
@@ -57,4 +65,5 @@ Partial Class Form3
 
     Friend WithEvents Button1 As Button
     Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents MonthCalendar1 As MonthCalendar
 End Class
